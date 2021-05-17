944dd87dc55d7d512681e09b34a5679aa74515c7
ChangminYi <jerry9803@gmail.com>
2021-05-17 15:29:56 +0900

    Terminal to Parsing table map added

--- compiler-syntax-analyzer/compiler-syntax-analyzer/main.cpp
+++ compiler-syntax-analyzer/compiler-syntax-analyzer/main.cpp
@@ -7,9 +7,11 @@ using namespace std;
 
 const string parse_table_name = "SLR_parsing_table.txt";
 const string cfg_table_name = "ambiguity_fixed_CFG.txt";
-string input_file_name = "test.txt";
+const string input_file_name = "test.txt";
 
-int main() {
+int main(/*int argc, char *argv[]*/) {
+	//input_file_name = argv[1];
+
 	initRevConvert();
 
 	vector<map<string, string>> parse_table = LoadParsingTable(parse_table_name);
