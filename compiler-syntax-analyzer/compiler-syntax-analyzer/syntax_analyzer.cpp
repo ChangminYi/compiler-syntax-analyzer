88a23fcd54294ec698e474083545cf301986d295
tjwjdgus12 <01048733438a@gmail.com>
2021-05-18 17:09:26 +0900

    syntax_analyze is implemented

--- compiler-syntax-analyzer/compiler-syntax-analyzer/syntax_analyzer.cpp
+++ compiler-syntax-analyzer/compiler-syntax-analyzer/syntax_analyzer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include "cfg_loader.h"
 #include "table_loader.h"
 #include "input_loader.h"
@@ -7,7 +8,7 @@
 using namespace std;
 
 const string parse_table_name = "SLR_parsing_table.txt";
-const string cfg_table_name = "ambiguity_fixed_CFG.txt";
+const string cfg_table_name = "cfg.txt";
 const string input_file_name = "test.txt";
 
 int main(/*int argc, char *argv[]*/) {
@@ -19,6 +20,40 @@ int main(/*int argc, char *argv[]*/) {
 	vector<Transition> cfg = LoadCFG(cfg_table_name);
 	list<Token> token_input = getInput(input_file_name);
 
+	stack<int> state_stack;
+	state_stack.push(0);
+	string next_symbol;
+	next_symbol = tokenToTerminal(token_input.front());
+	token_input.pop_front();
+
+	while(!token_input.empty() && !state_stack.empty()){
+		string act = parse_table[state_stack.top()][next_symbol];
+
+		if (act[0] == 's') {
+			next_symbol = tokenToTerminal(token_input.front());
+			token_input.pop_front();
+			int new_state = stoi(act.substr(1));
+			state_stack.push(new_state);
+		}
+		else if (act[0] == 'r') {
+			Transition transition = cfg[stoi(act.substr(1))];
+			next_symbol = transition.lhs;
+			for(int i = 0; i < transition.rhs_len; ++i){
+				state_stack.pop();
+			}
+		}
+		else if (act[0] == 'e') {
+			cerr << "Rejected\n"; // why? where?
+			exit(-1);
+		}
+		else if (act == "acc") {
+			cout << "Accepted\n";
+		}
+		else {
+			int new_state = stoi(act);
+			state_stack.push(new_state);
+		}
+	}
 
 	/* flow
 	stack<int> stk;
